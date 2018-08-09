e9cdeb5df7a05422c46161df9d5ebe600283d7b1
Kumail Naqvi <kumailmn@gmail.com>
2018-08-09 19:05:20 -0400

    update Sprial_Matrix

--- Python/Spiral_Matrix_Traversal.py
+++ Python/Spiral_Matrix_Traversal.py
@@ -12,33 +12,46 @@ def spiral(matrix):
   traversal = []
   offset = 0
   total = 0
-  for i in range(len(matrix)):
-    for j in range(len(matrix)):
-      total += 1
-      if(direction == 'R'):
-        print(offset)
-        if(j == (len(matrix) - 1 - offset)):
-          traversal += ['X']
-          direction = 'D'
+  x = 0
+  y = 0
+  for i in range(len(matrix) * len(matrix)):
+    print("Off", offset, traversal)
+    total += 1
+    if(direction == 'R'):
+        traversal += [matrix[x][y]]
+        if(y == (len(matrix) - 1 - offset)):
+            print("Item", matrix[x][y])
+            #traversal += ['X']
+            direction = 'D'
+            x += 1
         else:
-          traversal += [matrix[i + offset][j - offset]]
-      elif(direction == 'D'):
-        traversal += [matrix[j][len(matrix) - 1 - offset]]
-        if(j == (len(matrix) - 1 - offset)):
-          traversal += ['X']
-          direction = 'L'
-      elif(direction == 'L'):
-        traversal += [matrix[len(matrix) - 1 - offset][-1 * j - 1]]
-        if(j == (len(matrix) - 1 - offset)):
-          traversal += ['X']
-          direction = 'U'
-      elif(direction == 'U'):
-        traversal += [matrix[-1 * j - 1][offset]]
-        if(j == (len(matrix) - 1 - offset)):
-          traversal += ['X']
-          direction = 'R'
-          offset += 1
-  print(total)
+            y += 1
+        print("x and y", x,y)
+    elif(direction == 'D'):
+        traversal += [matrix[x][y]]
+        if(x == (len(matrix) - 1 - offset)):
+            #traversal += ['X']
+            direction = 'L'
+            y -= 1
+        else:
+            x += 1
+    elif(direction == 'L'):
+        traversal += [matrix[x][y]]
+        if(y == offset):
+            #traversal += ['X']
+            direction = 'U'
+            offset += 1
+            x -= 1
+        else:
+            y -= 1
+    elif(direction == 'U'):
+        traversal += [matrix[x][y]]
+        if(x == offset):
+            #traversal += ['X']
+            direction = 'R'
+            y += 1
+        else:
+            x -= 1
   return traversal
 
 print(spiral(test))
