a115c8805a32a71b4d9ee84d731a6b6d1a1bd74f
Kumail Naqvi <kumailmn@gmail.com>
2018-10-08 21:17:23 -0400

    Update

---
+++ Python/Merge_Linked_List.py
@@ -0,0 +1,24 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def mergeList(a, b):
+    if (not a or b and a.val > b.val): a, b = b, a
+    if a: a.next = mergeList(a.next, b)
+    return a
+
+a = ListNode(7)
+a.next = ListNode(32)
+a.next.next = ListNode(2)
+
+b = ListNode(5)
+b.next = ListNode(0)
+b.next.next = ListNode(43)
+
+def printLinkedList(ll):
+    while(ll):
+        print(ll.val)
+        ll = ll.next
+
+printLinkedList(mergeList(a, b))
--- Python/test.c
+++ Python/test.c
@@ -8,7 +8,28 @@
 int num = 12;
 
 main(){
-    int age = 3;
-    printf("print this\n\n");
-    printf("I am %d years old!", age);
+    int age = 4;
+    // printf("print this\n\n");
+    // printf("I am %d years old!", age);
+
+    // if(age == 6){
+    //     printf("NEW AGE1: %d", age);
+    // }
+    // else if(age = 7){
+    //     printf("NEW AGE2: %d", age);
+    // }
+    float ff = 2.12345678;
+    int d = 4;
+    printf("Floaty: %d\n", ff);
+    switch(age){
+        case 1:
+            printf("one");
+            break;
+        case 3 ... 5:
+            printf("3 to 5");
+            break;
+        default:
+            printf("REFWE");
+
+    };
 }
