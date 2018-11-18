613b5c6b66221b27bad6780d638478365b07c56b
Kumail Naqvi <kumailmn@gmail.com>
2018-11-17 20:13:15 -0500

    Update comments

--- Python/Longest_unique_substring.py
+++ Python/Longest_unique_substring.py
@@ -1,4 +1,7 @@
-#Basically a modified Kadanes algorithm
+#Question: Given a string find the largest substring that consists of only unique characters
+#Solution: Modified Kadanes algorithm
+#Difficulty: Medium 
+
 def lengthOfLongestSubstring(s):
         """
         :type s: str
@@ -8,13 +11,12 @@ def lengthOfLongestSubstring(s):
         if not s: return 0
         #Let global max and global min the first item
         cmax, gmax = [s[0]], [s[0]]
-        #For i in range from the second element to last
+        #For i in range from the second element to last, since 0th item is already in cmax and gmax
         for i in range(1, len(s)):
             #If element is not in the current max, then append
             if s[i] not in cmax: cmax += [s[i]]
-            #Otherwise, delete everything in current max up to the current element, and append the current element
-            else: 
-                cmax = cmax[cmax.index(s[i]) + 1:] + [s[i]]
+            #Otherwise, delete everything in current max up to (but not including) the current element, and append the current element
+            else: cmax = cmax[cmax.index(s[i]) + 1:] + [s[i]]
             #If current max is greater than global max, set global to current
             if len(cmax) > len(gmax): gmax = cmax
         return len(gmax)
