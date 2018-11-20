d29f392cd68db710e2f082a5fb5c951ca349e2fd
Kumail Naqvi <kumailmn@gmail.com>
2018-11-19 21:12:33 -0500

    Add

---
+++ Python/Graph_Is_Tree.py
@@ -0,0 +1,36 @@
+#Question: Given the number of nodes in a graph and the edges, determing if the graph is also a tree
+#Solution: Determine if the graph has any clycles in it (using union-find), if it does, it can't be a tree
+#Difficulty: Medium
+
+def validTree(n, edges):
+    """
+    :type n: int
+    :type edges: List[List[int]]
+    :rtype: bool
+    """
+    #Create a list where each item is the parent of itself
+    parent = list(range(n))
+    #Helper function to find the greatest parent of a node
+    def findParent(x): 
+        #If the node is it's own parent return itself, otherwise find parent of parent[node]
+        return x if parent[x] is x else findParent(parent[x])
+    for edge in edges:
+        #x is parent of edge_from y is parent of edge_to
+        x, y = findParent(edge[0]), findParent(edge[1])
+        #If both the edge_from and edge_to have the same parent they create a cycle that goes from edge_from to parent to edge_to, so the graph can't be a tree
+        if x is y: return False
+        #Otherwise set the parent of the edge_from to be edge_to, note you could also do this the other way around
+        parent[x] = y
+    #Make sure that number of edges is 1 less than number of nodes to ensure nothing is in a cycle
+    return len(edges) == n - 1
+
+def main():
+    a = [[0,1], [0,2], [0,3], [1,4]]
+    an = 5
+
+    b = [[0,1], [1,2], [2,3], [1,3], [1,4]]
+    bn = 5
+
+    print(validTree(an, a))
+    print(validTree(bn, b))
+main()
