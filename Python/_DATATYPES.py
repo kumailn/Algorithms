08e7789450311d45b336d12ab433ec968a6b5797
Kumail Naqvi <kumailmn@gmail.com>
2018-11-23 17:48:21 -0500

    Update class

--- Python/_DATATYPES.py
+++ Python/_DATATYPES.py
@@ -2,12 +2,19 @@
 
 #Definition for a binary tree node.
 class TreeNode:
+    inOrder = []
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
     def __str__(self):
         return str(self.val)
+    def printInOrder(self, r):
+        if r.left: r.printInOrder(r.left)
+        self.inOrder += [r.val]
+        if r.right: r.printInOrder(r.right)
+        return self.inOrder
+
 
 # Definition for singly-linked list.
 class ListNode:
