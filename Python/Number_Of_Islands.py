d3dba94eb5f3b4ab042eaa848e4371a635fcb05c
Kumail Naqvi <kumailmn@gmail.com>
2018-12-25 14:00:45 -0500

    update comments

--- Python/Number_Of_Islands.py
+++ Python/Number_Of_Islands.py
@@ -1,8 +1,8 @@
+#Question: Given a grid of 1s and 0s, determine the number of disjoint islands present if islands are connected by 1s
+#Solution: Run a DFS on each island, making what you have covered
+#Difficulty: Easy
+
 def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
         c = 0
         def dfs(i, j, grid):
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == "0": return
