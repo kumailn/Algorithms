22b8bdc9d6f7aa602d1803fb44c49d7ccb7f5a1a
Kumail Naqvi <kumailmn@gmail.com>
2019-01-05 22:57:57 -0500

    add topological sort python

---
+++ Python/Topological_Sort.py
@@ -0,0 +1,23 @@
+def topSort(numEdges, edges):
+    graph = {}
+    def depthFirst(current, visited, visitedNodes, graph):
+        visited.add(current)
+        for edge in graph[current]:
+            if not (edge in visited):
+                depthFirst(edge, visited, visitedNodes, graph)
+        visitedNodes.append(current)
+    for i in range(numEdges): graph[i] = []
+    for x, y in edges: graph[y] += [x]
+    visited = set(); result = []
+    for node in graph:
+        if not (node in visited):
+            visitedNodes = []
+            depthFirst(node, visited, visitedNodes, graph)
+            for node2 in visitedNodes: result.insert(0, node2)
+    return result
+
+def main():
+    graph = [[0, 5], [0, 4], [1, 4], [2, 5], [3, 2], [1, 3]]
+    print(topSort(6, graph))
+
+main()
