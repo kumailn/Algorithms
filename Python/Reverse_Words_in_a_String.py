4275fe9be94263ebbdc3ef5283ce275adae994dd
Kumail Naqvi <kumailmn@gmail.com>
2019-02-16 21:28:27 -0500

    update comments

--- Python/Reverse_Words_in_a_String.py
+++ Python/Reverse_Words_in_a_String.py
@@ -1,35 +1,43 @@
-#Example
-# Input:  ["t","h","e"," ","s","k","y"," ","i","s"," ","b","l","u","e"]
-# Output: ["b","l","u","e"," ","i","s"," ","s","k","y"," ","t","h","e"]
+#Question: Given a string, reverse the positions of the words, for example 'hi my name is' becomes 'is name my hi'
+#Solution: Reverse string, then reverse each word
+#Time Complexity: O(n)
 
-def reverseWords(self, strin):
+def reverseWords(strin):
         """
         :type str: List[str]
         :rtype: void Do not return anything, modify str in-place instead.
         """
+
+        #Helper function to reverse items in a list from a start index to an end index
         def rev(start, end, stringList):
+            #As long as the start index is less than the end index, swap the items at those indices and increment and decrement the indexs respectively 
             while start < end:
                 stringList[start], stringList[end] = stringList[end], stringList[start]
-                start += 1
-                end -= 1
+                start += 1; end -= 1
             return stringList
-        strin = strin[::-1]
+        
+        #Reverse the input string 
+        strin[:] = strin[::-1]
         i = 0
+
         while i < len(strin):
             end = i
+            #If the letter we're at is not a space, move up end to the next space
             if strin[i] != " ": 
                 while end + 1 < len(strin) and strin[end] != " ": 
                     if strin[end + 1] == " ": break
                     end += 1
+            #Reverse the string starting at index i to index end
             strin[:] = rev(i, end, strin)
+            #Move i up to the next letter after the space
             i = end + 1
-        
     
-#Crazy 4 line solution
-
-def rev(start, end, strin):
-    while start < end: strlin[start], strlin[end], start, end = strlin[end], strlin[start], start + 1, end - 1
-    strin[:], start = strin[::-1], 0
-    for i in range(len(strin)): a, start = rev(start, i - 1 if strin[i] == " " else i if len(strin) - 1 == i else start, strin), i + 1 if strin[i] == " " else start
-
+        return strin
         
+def main():
+    s = list("hi my name is")
+    reverseWords(s)
+
+    print(s)
+
+main()
