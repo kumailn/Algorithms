4f3cb89f9b515f16b958fc09c6d2d309ab562c95
Kumail Naqvi <kumailmn@gmail.com>
2019-02-24 14:05:31 -0500

    add merge_sort.py

--- Python/Merge_Sort.py
+++ Python/Merge_Sort.py
@@ -1,1 +1,28 @@
-def sortMerge(inputList):
+def sortMerge(nums):
+    #Our base case, if the list is 1 item long we can return itself because it's already sorted
+    if len(nums) == 1: return nums
+
+    #Recursively merge sort left and right lists
+    left = sortMerge(nums[:len(nums) // 2])
+    right = sortMerge(nums[len(nums) // 2:])
+
+    sortedNums, i, j = [], 0, 0
+
+    #While i and j are less than he lengths of each of their lists
+    while i < len(left) and j < len(right):
+        #Add the smaller item in the left list or right list and increment our counter
+        if left[i] < right[j]: 
+            sortedNums += [left[i]]; i += 1
+        else:
+            sortedNums += [right[j]]; j += 1
+
+    #Add the rest of the numbers as we've exhausted one of the lists
+    sortedNums += left[i:] + right[j:]
+
+    return sortedNums
+
+def main():
+    nums = [6, 7, 3, 2, 9, 3, 1, 0]
+    print(sortMerge(nums))
+
+main()
