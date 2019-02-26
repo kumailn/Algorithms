ca0139901a5f2603e2d5289d93176ed455df6e27
Kumail Naqvi <kumailmn@gmail.com>
2019-02-25 22:19:23 -0500

    Add subset_sum.py

--- Python/Subset_Sum.py
+++ Python/Subset_Sum.py
@@ -1,11 +1,18 @@
 def subsetSum(nums, sumTo):
-    if(len(nums) == 0):
-        return 0
-    elif(sumTo == 0):
-        return 1
-    else:
-        subsetSum()
+    sumleft = sumTo
+    store = [[0 for i in range(sumTo + 1)] for j in range(len(nums) + 1)]
+    print(store)
 
-print('hi')
+    for i in range(1, len(nums) + 1):
+        for w in range(1, sumTo + 1):
+            if w > sumleft:
+                store[i][sumleft] = store[i - 1][w]
+            else:
+                store[i][sumleft] = max(store[i - 1][sumleft], w + store[i - 1][sumleft - w])
+    print(store)
+    return store[len(nums)][sumTo]
 
-#Unifished
+def main():
+    print(subsetSum([3, 34, 4, 12, 5, 2], 9))
+
+main()
