6a8d67c980eb42949df8d500d58064f99a2fa4ff
Kumail Naqvi <kumailmn@gmail.com>
2019-03-01 09:23:01 -0500

    update comments

--- Python/Decode_Ways.py
+++ Python/Decode_Ways.py
@@ -1,36 +1,32 @@
-'''Given a decoding for each letter in the alphaber (A-> 1, ... Z-> 26) determine
-the number of ways a given string of digits could be interpereted'''
+#Question: Given a decoding for each letter in the alphabet (A-> 1, ... Z-> 26) determine the number of ways a given string of digits could be interpereted
+#Solution: Recursively compute all possibilities using dynamic programming
+#Difficulty: Medium
 
-# Generic recursive solution
-def numDecodings(inputDigits, l):
-    startIndex = len(inputDigits) - l
-    if(l == 0):
-        return 1
-    if(inputDigits[startIndex] == '0'):
-        "ONEE"
-        return 0
-    res = numDecodings(inputDigits, l - 1)
-    if(l >= 2 and int(inputDigits[startIndex] + inputDigits[startIndex + 1]) <= 26):
-        res += numDecodings(inputDigits, l - 2)
-    return res
+def numDecodings(s, l, store):
 
-# Recursive solution with memoization
-def numDecodingsDP(s, l, store):
+    #Set the index we're starting from to be the length of the string - the length we're at
     startIndex = len(s) - l
-    if(l == 0):
-        return 1
-    if(s[startIndex] == '0'):
-        return 0
-    if(store[l] != None):
-        return store[l]
-    res = numDecodingsDP(s, l - 1, store)
-    if(l >= 2 and int(s[startIndex] + s[startIndex + 1]) <= 26):
-        res += numDecodingsDP(s, l - 2, store)
+
+    #Recursive base cases -- If the length we're at is 0 return 1 (for 1 possibility)
+    if(l == 0): return 1
+    #If s starts with a 0 then it's invalid
+    if(s[startIndex] == '0'): return 0
+    #If we're already computed the possibilites at index l, return it
+    if(store[l]): return store[l]
+    
+    #Recursively compute the number of decodings by subtracting 1 from the length 
+    res = numDecodings(s, l - 1, store)
+
+    #Recursively compute the number of decodings by subtracting 2 from the length and also cecking if those 2 numbers are <= 26 
+    if(l >= 2 and int(s[startIndex] + s[startIndex + 1]) <= 26): res += numDecodings(s, l - 2, store)
+
+    #Store the result in our dynamic programs array
     store[l] = res
+
+    #Return the result
     return res  
 
-data = '11111111111111111111'
-print(numDecodings(data, len(data)))
-print(numDecodingsDP(data, len(data), [None for i in range(len(data) + 1)]))
-
-
+def main():
+    data = '12'
+    print(numDecodings(data, len(data), [None] * (len(data) + 1)))
+main()
