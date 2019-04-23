1d666c0c2cbd1bef16e61fd2ab070210b4fad800
Kumail Naqvi <kumailmn@gmail.com>
2019-04-23 13:55:36 -0400

    add maximum_subaray py

---
+++ Python/Maximum_Subarray.py
@@ -0,0 +1,25 @@
+#Question: Find the sum of a continuous subarray in an array with the largest sum
+#Solution: Kadane's algorithm (explained below)
+#Difficulty: Easy
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+   
+from typing import List
+
+def maxSubArray(nums: List[int]) -> int:
+        #Initialize a store for the local and global maximum sumarrays
+        localMax = globalMax = float('-inf')
+        
+        for i, v in enumerate(nums):
+                #LocalMax becomes the larger of the current number or the current number + localMax itself
+                localMax = max(v, v + localMax)
+
+                #If localMax happens to become larger than globalMax, set globalMax to localMax
+                if localMax > globalMax: globalMax = localMax
+        return globalMax
+
+def main():
+    #Result should be 6
+    print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+main()
