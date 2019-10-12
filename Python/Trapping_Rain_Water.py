31d71de98bb668f6fd7d87ed373d9d6676290fe3
Kumail <kumailmn@gmail.com>
2019-10-12 15:12:29 -0400

    update Trapping_Rain_Water.py

--- Python/Trapping_Rain_Water.py
+++ Python/Trapping_Rain_Water.py
@@ -1,25 +1,25 @@
-# Question: Determing how much rain water is trapped in a stage of blocks 
+# Question: Determing how much rain water is trapped in a stage of blocks
 # Solution: Use two pointers to determine the lower bound and shift accordingly
 # Difficulty: Hard
 
-from typing import List
 
 def trap(height: List[int]) -> int:
-    water = lastLevel = 0
-    left, right = 0, len(height) - 1
+    water = smaller = depth = 0
+    l, r = 0, len(height) - 1
 
-    while left < right:
-        if height[left] <= height[right]:
-            smallerBound = height[left]; left += 1
+    while l < r:
+        # Set 'smaller' to be the smaller height of the two pointers, and shift pointers accordingly
+        if height[l] < height[r]:
+            smaller = height[l]
+            l += 1
         else:
-            smallerBound = height[right]; right -= 1
-        
-        if smallerBound > lastLevel: lastLevel = smallerBound
-        water += lastLevel - smallerBound
+            smaller = height[r]
+            r -= 1
+
+        # Set depth to be the largest lower bound we've encountered so far
+        # this will be the upper bound of the level of the water
+        depth = max(depth, smaller)
+
+        # The water level per block will simply be the depth we've computed minus the smaller bound we're currently at
+        water += depth - smaller
     return water
-
-def main():
-    height = [0,1,0,2,1,0,1,3,2,1,2,1]
-    print(trap(height))
-
-main()
